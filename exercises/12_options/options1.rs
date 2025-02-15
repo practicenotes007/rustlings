5d627fd67bcfe0d4a742cd74c92347567344bf25
practicenotes007 <practicenotes@163.com>
2025-02-15 20:10:56 +0800

    ch12 options

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -3,7 +3,13 @@
 // someone eats it all, so no icecream is left (value 0). Return `None` if
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
-    // TODO: Complete the function body.
+    if hour_of_day > 23 {
+        None
+    } else if hour_of_day >= 22 {
+        Some(0)
+    } else {
+        Some(5)
+    }
 }
 
 fn main() {
@@ -16,10 +22,7 @@ mod tests {
 
     #[test]
     fn raw_value() {
-        // TODO: Fix this test. How do you get the value contained in the
-        // Option?
-        let icecreams = maybe_icecream(12);
-
+        let icecreams = maybe_icecream(12).unwrap(); // 使用 unwrap 获取 Option 中的值
         assert_eq!(icecreams, 5); // Don't change this line.
     }
 
--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -7,10 +7,10 @@ struct Point {
 fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
 
-    // TODO: Fix the compiler error by adding something to this match statement.
+    // 修复编译器错误，处理 `Some` 和 `None` 的情况
     match optional_point {
         Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
-        _ => panic!("No match!"),
+        None => println!("No point found!"),
     }
 
     println!("{optional_point:?}"); // Don't change this line.
